97083789fc8fc83e7f2d00ddda54e3271b59e7ca
Mireille Bobbert <mlfbobbert@gmail.com>
2018-04-04 13:15:50 +0200

    changed link to stylesheet

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Cinema Booking </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="index.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
     <!--<script src="main.js"></script>-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
